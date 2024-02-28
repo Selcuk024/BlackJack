4f85807231684069af2f759ddc58c1cbeb0bb3bc
Selcuk <1202320@student.roc-nijmegen.nl>
2024-02-28 13:15:08 +0100

    test changes

--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("this is a test, write down something");
+            string message = Console.ReadLine();
+            Console.WriteLine("you chose this, correct?:" + message);
         }
     }
 }
